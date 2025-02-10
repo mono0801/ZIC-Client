d212d75ff480b9415a9b43602f409fd93b43e2f5
sellen <austin0801@naver.com>
2025-02-11 01:36:57 +0900

    url 설명 추가

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,12 @@ const JoinRoutes = () => {
     return (
         <Routes>
             <Route element={<JoinHeader />}>
+                {/* 회원 가입 대여자 || 이용자 고르는 페이지 */}
                 <Route path="/category" element={<JoinCategory />} />
+                {/* 이용자 & 대여자 회원 가입 정보 페이지 */}
                 <Route path="/:role/info" element={<JoinInfo />} />
             </Route>
+            {/* 회원가입 성공 페이지 */}
             <Route path="/:role/success" element={<JoinSuccess />} />
         </Routes>
     );
@@ -30,9 +33,13 @@ const JoinRoutes = () => {
 const UserRoutes = () => {
     return (
         <Routes>
+            {/* 연습실 내부 방 페이지 */}
             <Route path="/practiceRoom" element={<UserPracticeRoom />} />
+            {/* 결제 페이지 */}
             <Route path="/PracticeRoom/payment" element={<UserPayment />} />
+            {/* 예약 내역 페이지 */}
             <Route path="/reservation" element={<UserReservation />} />
+            {/* 마이 페이지 */}
             <Route path="/mypage" element={<User />} />
         </Routes>
     );
@@ -42,9 +49,12 @@ const OwnerRoutes = () => {
     return (
         <Routes>
             <Route element={<MainHeader />}>
+                {/* 대여자 메인 페이지 */}
                 <Route path="/" element={<Owner />} />
+                {/* 대여자 연습실 내부 방 추가 페이지 */}
                 <Route path="/revenue" element={<OwnerRevenue />} />
             </Route>
+            {/* 대여자 수익 페이지 */}
             <Route
                 path="/practiceRoom/:practiceRoomId"
                 element={<OwnerPracticeRoom />}
@@ -60,6 +70,7 @@ function App() {
                 <Route element={<RootContainer />}>
                     {/* 로그인 */}
                     <Route path="/" element={<Home />} />
+                    {/* 메인 페이지 */}
                     <Route path="/main" element={<PracticeRoom />} />
 
                     {/* 회원가입 */}
