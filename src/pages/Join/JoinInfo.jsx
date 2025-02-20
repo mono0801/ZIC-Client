15ea22214b881c09e659bf9640d3230a0bd2369f
hyeryunYou <152360502+hyeryunYou@users.noreply.github.com>
2025-02-20 19:46:37 +0900

    🔨 [Refactor] [#72] 회원가입 시 정보 입력 페이지 수정사항
    * :hammer: Refactor: 진행상황 커밋
    
    * 캘린더 진행상황 커밋
    
    * owner 페이지 진행상황 커밋
    
    * 캘린더 api 받아오기
    
    * owner페이제 진행상황 커밋
    
    * 캘린더 컴포넌트 해당 달 전체 예약 조회 api 연결
    
    * 캘린더 컴포넌트 tilecontent 변경
    
    * :sparkles: 캘린더 수정
    
    * :hammer: Refactor: 예약내역날짜 밑밑 파란점 표시 기능 추가
    
    * :sparkels: feature [#42] 달력 컴포넌트 api 연결 완료
    
    * :sparkles: [#42] 달력 컴포넌트 구현 완성
    
    * :sparkles: feature [#42] 달력 컴포넌트 구현 및 api연결 완성
    
    * 유저 예약 취소 api 연결 구현
    
    * 캘린더 컴포넌트 구현
    
    * 유저 예약 내역 페이지지
    
    * 예약내역 카드드
    
    * :sparkles: feature [#42] 캘린더 컴포넌트 구현 완료
    
    * :sparkles: feature [#69] 이용자 메인 페이지 api연결 완료
    
    * :sparkles: feature [#32] 이용자 예약 내역 페이지 및 예약 취소 api 연결 완료
    
    * :hammer: Refactor [#72] JoinInfo 페이지 placeholder 수정 및 지역 리스트 수정
    
    ---------
    
    Co-authored-by: qbnyp <moneycloudy@naver.com>

--- src/pages/Join/JoinInfo.jsx
+++ src/pages/Join/JoinInfo.jsx
@@ -1,24 +1,24 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Button from "../../Components/Button";
 import { JoinContainer } from "../../styles/container";
 import styled from "styled-components";
+import { IoIosSearch } from "react-icons/io";
 import { regions, instruments } from "../../assets/category";
 import ScrollContainer from "react-indiana-drag-scroll";
-import Isearch from "../../Components/icons/Isearch";
-import axios from "axios";
 
 const InputContainer = styled.div`
     width: 100%;
     height: 20%;
+    margin-bottom: 60%;
 `;
 
 const InputLabel = styled.p`
-    font-size: 80%;
+    font-size: 100%;
     font-family: "Pretendard-Regular";
     letter-spacing: 2%;
-    margin-top: 4%;
-    margin-bottom: 1%;
+    margin-top: 5%;
+    margin-bottom: 2%;
     color: ${(props) => (props.isActive ? "#C6C6C6" : "#030303")};
 `;
 
@@ -31,7 +31,7 @@ const InputWrapper = styled.div`
         background: none;
         border: none;
         border-bottom: 1px solid #dcdcdc;
-        padding: 1% 0 2% 1%;
+        padding: 2% 0 2% 1%;
         box-sizing: border-box;
         font-size: 100%;
         font-family: "Pretendard-Regular";
@@ -42,6 +42,8 @@ const InputWrapper = styled.div`
     svg {
         position: absolute;
         color: #c6c6c6;
+        width: 1.7em;
+        height: 1.7em;
         top: 50%;
         transform: translateY(-50%);
         right: 0%;
@@ -52,7 +54,7 @@ const InputWrapper = styled.div`
 const RegionCategory = styled(ScrollContainer)`
     width: 100%;
     max-width: 100%;
-    height: 2rem;
+    height: 2.3rem;
     margin-top: 2%;
     overflow-x: auto;
     display: flex;
@@ -68,7 +70,7 @@ const RegionCategory = styled(ScrollContainer)`
 `;
 
 const RegionBtn = styled.button`
-    width: 18%;
+    width: 20%;
     background-color: ${(props) => (props.selected ? "#278cff" : "#ffffff")};
     border: 1px solid ${(props) => (props.selected ? "#278cff" : "#dcdcdc")};
     border-radius: 2rem;
@@ -93,8 +95,7 @@ const InstrumentContainer = styled.div`
         font-size: 100%;
         font-family: "Pretendard-Regular";
         letter-spacing: 2%;
-        margin-top: 1%;
-        margin-bottom: 4%;
+        margin-bottom: 5%;
         color: ${(props) => (props.isActive ? "#C6C6C6" : "#030303")};
     }
 `;
@@ -120,109 +121,18 @@ const InstrumentBtn = styled.button`
 const JoinInfo = () => {
     const role = useParams();
     const navigate = useNavigate();
+    const [regionToggle, setRegionToggle] = useState(false);
     const [region, setRegion] = useState("");
     const [brand, setBrand] = useState("");
     const [number, setNumber] = useState("");
     const [address, setAddress] = useState("");
     const [selectedInstruments, setSelectedInstruments] = useState([]);
-    const [practiceRoomId, setPracticeRoomId] = useState(0);
-
-    useEffect(() => {
-        console.log("내가 원하는 값 : " + practiceRoomId);
-    }, [region, brand, number, address, selectedInstruments, practiceRoomId]);
 
     const handleNext = () => {
         console.log(region);
         console.log(selectedInstruments);
-
-        if (role.role === "user") {
-            userSignup()
-                .then(() => {
-                    navigate(`/join/${role.role}/success`);
-                })
-                .catch((error) => console.error("회원가입 실패:", error));
-        }
-
-        if (role.role === "owner") {
-            ownerSignup()
-                .then(() => {
-                    navigate(`/join/${role.role}/success`, {
-                        state: { practiceRoomId },
-                    });
-                })
-                .catch((error) => console.error("회원가입 실패:", error));
-        }
-    };
-
-    const userSignup = async () => {
-        console.log(localStorage.getItem("accessToken"));
-        try {
-            const res = await axios.patch(
-                `${import.meta.env.VITE_API_URL}/api/user/details`,
-                {
-                    region: region,
-                    instrumentList: selectedInstruments,
-                },
-                {
-                    headers: {
-                        Authorization: localStorage.getItem("accessToken"),
-                    },
-                }
-            );
-            console.log("회원가입 응답:", res.data);
-            console.log(res.data.isSuccess);
-            if (res.data.isSuccess) {
-                const { userId, userName, userRole, token } = res.data.result;
-
-                localStorage.setItem("accessToken", token);
-                localStorage.setItem("userId", userId);
-                localStorage.setItem("userName", userName);
-                localStorage.setItem("userType", userRole);
-                console.log("localStorage에 저장 완료!");
-            }
-        } catch (error) {
-            console.error("데이터를 연결하는 중 에러 발생:", error);
-        }
-    };
-
-    const ownerSignup = async () => {
-        console.log(localStorage.getItem("accessToken"));
-        try {
-            const res = await axios.patch(
-                `${import.meta.env.VITE_API_URL}/api/owner/details`,
-                {
-                    region1: region, //통일 필요
-                    region2: address,
-                    instrumentList: selectedInstruments,
-                    businessName: brand,
-                    businessNumber: number,
-                },
-                {
-                    headers: {
-                        Authorization: localStorage.getItem("accessToken"),
-                    },
-                }
-            );
-            console.log("회원가입 응답:", res.data);
-            console.log(res.data.isSuccess);
-            if (res.data.isSuccess) {
-                const { userId, userName, userRole, token, practiceRoomId } =
-                    res.data.result;
-
-                localStorage.setItem("accessToken", token);
-                localStorage.setItem("userId", userId);
-                localStorage.setItem("userName", userName);
-                localStorage.setItem("userType", userRole);
-                localStorage.setItem("practiceRoomId", practiceRoomId);
-
-                console.log("localStorage에 저장 완료!");
-            }
-            setPracticeRoomId(res.data.result.practiceRoomId);
-            console.log(practiceRoomId);
-            console.log(res.data.result.practiceRoomId);
-        } catch (error) {
-            console.error("데이터를 연결하는 중 에러 발생:", error);
-        }
+        // TODO : 로그인 API 설정하기
+        navigate(`/join/${role.role}/success`);
     };
 
     const handleInstrumentClick = (instrument) => {
@@ -242,24 +152,27 @@ const JoinInfo = () => {
                     <InputWrapper>
                         <input
                             type="text"
-                            placeholder="도로명, 지번, 건물명 검색"
+                            placeholder="시/도 단위 검색"
                             value={region}
                             onChange={(e) => setRegion(e.target.value)}
                         />
-                        <Isearch />
+                        <IoIosSearch
+                            onClick={() => setRegionToggle(!regionToggle)}
+                        />
                     </InputWrapper>
-
-                    <RegionCategory>
-                        {regions.map((el) => (
-                            <RegionBtn
-                                key={el}
-                                selected={region == el}
-                                onClick={() => setRegion(el)}
-                            >
-                                {el}
-                            </RegionBtn>
-                        ))}
-                    </RegionCategory>
+                    {regionToggle ? (
+                        <RegionCategory>
+                            {regions.map((el) => (
+                                <RegionBtn
+                                    key={el}
+                                    selected={region == el}
+                                    onClick={() => setRegion(el)}
+                                >
+                                    {el}
+                                </RegionBtn>
+                            ))}
+                        </RegionCategory>
+                    ) : null}
                 </InputContainer>
             ) : (
                 <InputContainer>
@@ -287,29 +200,19 @@ const JoinInfo = () => {
                     <InputWrapper>
                         <input
                             type="text"
-                            placeholder="도로명, 지번, 건물명 검색"
+                            placeholder="시/도 단위 검색"
                             value={region}
                             onChange={(e) => setRegion(e.target.value)}
                         />
-                        <Isearch />
+                        <IoIosSearch />
                     </InputWrapper>
-                    <RegionCategory>
-                        {regions.map((el) => (
-                            <RegionBtn
-                                key={el}
-                                selected={region == el}
-                                onClick={() => setRegion(el)}
-                            >
-                                {el}
-                            </RegionBtn>
-                        ))}
-                    </RegionCategory>
                     <InputWrapper>
                         <input
                             type="text"
                             placeholder="상세주소를 작성해주세요."
                             value={address}
                             onChange={(e) => setAddress(e.target.value)}
+                            style={{ marginTop: "5%" }}
                         />
                     </InputWrapper>
                 </InputContainer>
